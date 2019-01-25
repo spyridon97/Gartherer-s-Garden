26fd55a409911d0cd194f1014d49169b2d27e873
tjgiannhs <36013878+tjgiannhs@users.noreply.github.com>
2019-01-25 21:34:55 +0200

    Now returns product and quantity
    Changed it to return a json of the product and its quantity

--- server/api/deleteFromCartOnce.php
+++ server/api/deleteFromCartOnce.php
@@ -11,7 +11,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 //  includes
 include_once 'apiDocumentation.php';
-
+include_once '../controllers/ProductsController.php';
 //  utilities
 $utilities = new Utilities();
 //  allow only POST Request
@@ -24,6 +24,10 @@ $db = $database->getConnection();
 
 //  get product id
 $data = json_decode(file_get_contents("php://input"));
+
+$results = array();
+$results["products"] = array();
+$results["quantities"] = array();
 
 // make sure we were given an id parameter
 if ($data->id > 0) {
@@ -43,8 +47,42 @@ if ($data->id > 0) {
             //  set response code - 200 OK
             http_response_code(200);
 
-            //  tell the user
-            echo json_encode(array("message" => "ProductsController successfully removed from cart once."));
+            $productsController = new ProductsController($db);
+
+            //  reading product
+
+            //  query product
+            $stmt = $productsController->getProduct($data->id);
+            $num = $stmt->rowCount();
+            
+            //  get retrieved row
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            //  return product json
+            $product_item = array(
+            "id" => intval($row['Id']),
+            "name" => html_entity_decode($row['Name']),
+            "price" => intval($row['Price']),
+            "type" => html_entity_decode($row['Type']),
+            "ad" => (file_exists("../../images/Ads/{$row['Image']}")) ? html_entity_decode("/images/Ads/{$row['Image']}") : "",
+            "bottle" => (file_exists("../../images/Bottles/{$row['Image']}")) ? html_entity_decode("/images/Bottles/{$row['Image']}") : "",
+            "sprite" => (file_exists("../../images/Sprites/{$row['Image']}")) ? html_entity_decode("/images/Sprites/{$row['Image']}") : "",
+            "quote" => html_entity_decode($row['Quote']),
+            "effect" => html_entity_decode($row['Effect']),
+            "casting_cost" => intval($row['Casting_Cost'])
+            );
+            
+            array_push($results["products"], $product_item);
+            $tmp = array_count_values($_SESSION["cart"]);
+            $quantity = $tmp[$data->id];
+
+            if($quantity!=NULL){
+                array_push($results["quantities"], $quantity);
+            }else{
+                array_push($results["quantities"], 0);
+            }
+            echo json_encode($results);
+            
         } else {
             //  set response code - 404 Not found
             http_response_code(404);
